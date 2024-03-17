6c770fe8dc3833b207c3098cc55a774c57252680
Adam Young <adamyoung1414@gmail.com>
2024-03-16 20:29:26 -0400

    modified displayLibrary to create an element for display on the page added two more sample books for a total of 3

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // variable to keep track of what is in the library
 const myLibrary = [];
 
+// container where books are displayed
+const libraryContainer = document.querySelector('#libraryContainer');
+
 // constructor function to create a book
 function Book(title, author, pages, read) {
     this.title = title;
@@ -23,15 +26,28 @@ function addBookToLibrary (book) {
 
 function displayLibrary () {
     myLibrary.forEach((item) => {
-        console.log(item.info());
+        // create element for each book
+        let newDiv = document.createElement('div');
+        // get the books information for display on the node
+        let bookInfo = document.createTextNode(item.info());
+        // append the book info to the new element
+        newDiv.appendChild(bookInfo);
+        // add a class to the new element for styling
+        newDiv.classList.add('book');
+        // append element to libraryContainer
+        libraryContainer.append(newDiv);
     })
 }
 
-// create a book
+// create sample books
 let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+let FotR = new Book("Fellowship of the Ring", "J.R.R. Tolkien", 493, false);
+let theTwoTowers = new Book("The Two Towers", "J.R.R. Tolkien", 352, false);
 
-// add book to library
+// add sample books to library
 addBookToLibrary(theHobbit);
+addBookToLibrary(FotR);
+addBookToLibrary(theTwoTowers);
 
-// display library
+// display sample books
 displayLibrary();
